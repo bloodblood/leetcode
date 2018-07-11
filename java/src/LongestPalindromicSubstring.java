1a603d317a889dafc623b5dad7aa78f98fc11434
InfiniteX <727160222@qq.com>
2018-07-11 18:43:21 +0800

    Reverse Integer... solved Longest Palindromic... solved

--- java/src/LongestPalindromicSubstring.java
+++ java/src/LongestPalindromicSubstring.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Given a string s, find the longest palindromic substring in s.
  * You may assume that the maximum length of s is 1000.
@@ -7,29 +9,26 @@
  */
 
 
-// TODO: 算法超时
+// TODO: Naive Solution
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-        String input = "babewrqwre";
+        String input = "cbbd";
         System.out.println(longestPalindrome(input));
     }
 
     // naive solution
     private static String longestPalindrome(String s) {
         char[] data = s.toCharArray();
-        int maxLength = 0;
+        int maxLength = 1;
         int start = 0;
         for (int i = 0; i < data.length; i++) {
-            for (int j = i; j < data.length; j++) {
-                int tempLength = j - i + 1;
-                char[] temp = new char[tempLength];
-                System.arraycopy(data, i, temp, 0, tempLength);
+            for (int length = maxLength; length <= data.length - i; length++) {
+                char[] temp = new char[length];
+                System.arraycopy(data, i, temp, 0, length);
                 boolean isPalindrome = checkPalindrome(temp);
                 if (!isPalindrome) continue;
-                if (maxLength < tempLength) {
-                    start = i;
-                    maxLength = tempLength;
-                }
+                start = i;
+                maxLength = length;
             }
         }
         return String.copyValueOf(data, start, maxLength);
@@ -37,7 +36,7 @@ public class LongestPalindromicSubstring {
 
     private static boolean checkPalindrome(char[] temp) {
         int length = temp.length;
-        for (int i = 0; i <= length / 2; i++) {
+        for (int i = 0; i <= length/2; i++) {
             if (temp[i] != temp[length - i - 1])
                 return false;
         }
---
+++ java/src/ReverseInteger.java
@@ -0,0 +1,25 @@
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * <p>
+ * Input: -120
+ * Output: -21
+ */
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int num = -231;
+        System.out.println(reverse(num));
+    }
+
+    private static int reverse(int x) {
+        int num = Math.abs(x);
+        int ans;
+        StringBuilder sb = new StringBuilder(String.valueOf(num)).reverse();
+        if (x < 0) sb.insert(0, '-');
+        try {
+            ans = Integer.valueOf(sb.toString());
+        } catch (NumberFormatException e) {
+            ans = 0;
+        }
+        return ans;
+    }
+}
