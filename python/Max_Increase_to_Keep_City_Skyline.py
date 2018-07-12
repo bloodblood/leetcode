9a9be434b229e44362ab36800f803c2399ddcc38
qzy <1319075971@qq.com>
2018-07-12 21:00:35 +0800

    添加了main函数

--- python/Max_Increase_to_Keep_City_Skyline.py
+++ python/Max_Increase_to_Keep_City_Skyline.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxIncreaseKeepingSkyline(self, grid):
         max_h = []
         max_v = []
-        sum_plus = 0;
+        sum_plus = 0
         for i in range(len(grid)):
              max_h.append(max(grid[i]))
         for i in range(len(grid[0])):
@@ -12,3 +12,8 @@ class Solution(object):
             for j in range(len(grid)):
                 sum_plus = sum_plus + min(max_h[i], max_v[j]) - grid[i][j]
         return sum_plus
+
+
+if __name__ == '__main__':
+    grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(Solution().maxIncreaseKeepingSkyline(grid))
