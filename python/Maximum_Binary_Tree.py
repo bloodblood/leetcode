07ed76761b165bb9ab9be436faa2cb45b9b8d454
qzy <1319075971@qq.com>
2018-08-11 13:23:41 +0800

    aa

--- python/Maximum_Binary_Tree.py
+++ python/Maximum_Binary_Tree.py
@@ -14,14 +14,17 @@ class Solution(object):
         """
         if not nums:
             return None
-        index = 0
-        maxnum = nums[0]
-        for i, e in enumerate(nums):
-            if e > maxnum:
-                maxnum = e
-                index = i
+        maxnum, index = self.findMax(nums)
         node = TreeNode(maxnum)
         node.left = self.constructMaximumBinaryTree(nums[:index])
         node.right = self.constructMaximumBinaryTree(nums[index + 1:])
         return node
 
+    def findMax(self, num):
+        index = 0
+        maxnum = num[0]
+        for i, e in enumerate(num):
+            if e > maxnum:
+                maxnum = e
+                index = i
+        return maxnum, index
