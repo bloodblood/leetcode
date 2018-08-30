d991b11d8b418e3dc5748e40168ef0d2025c251e
qzy <1319075971@qq.com>
2018-08-30 22:25:51 +0800

    www

--- python/Binary_Tree_Pruning.py
+++ python/Binary_Tree_Pruning.py
@@ -11,3 +11,23 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
+        self.reprune(root)
+        return root
+
+
+
+    def reprune(self, root):
+        if root is None:
+            return True
+        leftprune = True
+        rightprune = True
+        if root.left is not None:
+            leftprune = self.reprune(root.left)
+            if leftprune:
+                root.left = None
+        if root.right is not None:
+            rightprune = self.reprune(root.right)
+            if rightprune:
+                root.right = None
+        return root.val == 0 and leftprune and rightprune
+
